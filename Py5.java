6b33121d05a182fbe36db91096f0c1c010241e06
SDECRYPTO <83850994+SDECRYPTO@users.noreply.github.com>
2021-05-08 21:09:41 +0530

    Add files via upload

--- Py5.java
+++ Py5.java
@@ -1,20 +1,24 @@
 class Py5
 {
 	public static void main(String args[])
-	{	
-		for (int i = 9; i >= 1; i--)   
-		{  
-			for(int k = 1;k<i;k++)
-			{
-				System.out.print(" ");
-			}
-			for (int j = 1; j <= i; j++)  
-			{  
-				System.out.print(j+" ");  
-			}  
-				System.out.println();  
-		} 
-
+	{ 	
 		
+		for(int i=9;i>=1;i--)
+		{	
+			for(int k=1;k<i;k++)
+			{
+			System.out.print(" ");
+			}
+			
+			for(int j=i; j<=9;j++)
+			{
+				System.out.print(j);
+			}
+			for(int l=8; l>=i;l--)
+			{
+				System.out.print(l);
+			}
+				System.out.println();
+		}
 	}
 }
